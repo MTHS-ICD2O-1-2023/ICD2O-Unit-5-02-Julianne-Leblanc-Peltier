e7a8bd8759d26a6c11422c4b04dee7345540e3d0
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2024-04-24 18:48:17 +0000

    finished except storyboard

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Julianne Leblanc-Peltier
+// Created on: April 2024
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  const randomNumber = Math.floor(Math.random() * 6) + 1 // returns a random integer from 1 to 6 into variable "randomNumber"  
+  const alwaysOnButtonChecked = document.getElementById("option-positive").checked
+
+  if (alwaysOnButtonChecked == true) {
+    // output
+    document.getElementById("number-generated").innerHTML = "Your number is:" + randomNumber
+  } else {
+    // output
+    document.getElementById("number-generated").innerHTML = "Your number is:" + randomNumber * -1
+  }
+}
